1228ed3f146368ea1ead56db38099b51d9d0e40f
Artur Ciepielowski <artciepi@gmail.com>
2022-04-06 20:11:57 +0200

    Created Osoba

---
+++ src/main/java/com/sda/arppl2/methodology/Osoba.java
@@ -0,0 +1,12 @@
+package com.sda.arppl2.methodology;
+
+public class Osoba {
+
+    String imie;
+    String nazwisko;
+
+    public Osoba(String imie, String nazwisko){
+        this.imie = imie;
+        this.nazwisko = nazwisko;
+    }
+}
