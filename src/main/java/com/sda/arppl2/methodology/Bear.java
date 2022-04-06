10739e9211913c85dbd4c6fe5cb17bb92d705545
alicj <alicja.mojska@gmail.com>
2022-04-06 19:51:50 +0200

    pierwszy komit po sqashu :>

---
+++ src/main/java/com/sda/arppl2/methodology/Bear.java
@@ -0,0 +1,10 @@
+package com.sda.arppl2.methodology;
+
+public class Bear {
+    String imie;
+    int wiek;
+
+    public Bear(String imie, int wiek){
+        this.imie = imie;
+        this.wiek = wiek;
+}}
--- src/main/java/com/sda/arppl2/methodology/Main.java
+++ src/main/java/com/sda/arppl2/methodology/Main.java
@@ -2,7 +2,10 @@ package com.sda.arppl2.methodology;
 
 public class Main {
     public static void main(String[] args) {
-
-        System.out.println("Zadanie 1 wykonane!");
+        System.out.println("Zadanie 1 wykonane");
+        Bear p  = new Bear("Marian", 32);
+        Bear p2 = new Bear("Olek", 2);
+        Bear p3 = new Bear("Filipek", 10);
+        Bear p4 = new Bear("Teddy", 3);
     }
 }
